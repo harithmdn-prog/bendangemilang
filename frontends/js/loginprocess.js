b591fdaa2e0e3c1cf4b12cdb0140aa4a5063d9af
harithmdn <harithmdn@gmail.com>
2026-06-24 19:31:25 +0800

    update code

--- frontends/js/loginprocess.js
+++ frontends/js/loginprocess.js
@@ -10,7 +10,7 @@ adminLoginForm.addEventListener('submit', async (e) => {
 
     try {
 
-        const response = await fetch('http://localhost:5000/api/login', {
+        const response = await fetch('https://backend-bendangemilang.onrender.com/api/login', {
 
             method: 'POST',
             headers: {
