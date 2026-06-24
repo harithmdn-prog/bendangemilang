030ea0a58cbe985233138c3eaee3916520e1cb81
harithmdn <harithmdn@gmail.com>
2026-06-24 19:37:30 +0800

    Connect Vercel and Render

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const app = express()
 
 connectDB()
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://bendangemilang.vercel.app',
+
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
